2bcdb56f1127ac8ab41dd85f6ab2d291fb8c2b74
Lennart Krauch <lennart.krauch@inovex.de>
2023-03-24 13:35:21 +0100

    added definition for multExp

--- ast/ast.go
+++ ast/ast.go
@@ -41,20 +41,39 @@ func (plus_exp PlusExp) Eval() int {
 	return plus_exp.Left.Eval() + plus_exp.Right.Eval()
 }
 
+// pretty function for plus expression
 func (plus_exp PlusExp) Pretty() string {
 	return "(" + plus_exp.Left.Pretty() + "+" + plus_exp.Right.Pretty() + ")"
 }
 
+// define the mult expression
+// implicitly implements the Exp interface
+type MultExp struct {
+	Left  Exp
+	Right Exp
+}
+
+// eval function for mult expression
+// returns the value of the mult expression
+func (mult_exp MultExp) Eval() int {
+	return mult_exp.Left.Eval() * mult_exp.Right.Eval()
+}
+
+// pretty function for mult expression
+func (mult_exp MultExp) Pretty() string {
+	return "(" + mult_exp.Left.Pretty() + "*" + mult_exp.Right.Pretty() + ")"
+}
 func main() {
-	// create an int expression
+	// creating test expressions
 	int_exp := IntExp{Val: 5}
 	int_exp2 := IntExp{Val: 10}
 	int_exp3 := IntExp{Val: 15}
-
-	// create a plus expression
+	mult_exp := MultExp{Left: int_exp, Right: int_exp}
 	plus_exp := PlusExp{Left: int_exp, Right: int_exp}
 	plus_exp2 := PlusExp{Left: int_exp2, Right: int_exp3}
 	plus_exp3 := PlusExp{Left: plus_exp, Right: plus_exp2}
+	plus_exp3 = PlusExp{Left: mult_exp, Right: plus_exp3}
 	fmt.Print(plus_exp3.Pretty())
+	fmt.Println("=", plus_exp3.Eval())
 
 }
